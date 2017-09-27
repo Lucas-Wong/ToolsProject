4a838bbc3c92da206b28edccf2199cb96e857b3b
lucas.wang <lucas.wang@ariix.com>
2017-09-27 09:56:55 +0800

    Internet worm: cn163

---
+++ Practice/cn163.py
@@ -0,0 +1,69 @@
+#! /usr/bin/env python
+#  _*_coding:utf-8_*_
+'''
+document note
+'''
+import re
+import threading
+import time
+import os
+import requests
+
+PYTHONIOENCODING = 'utf-8'
+class Archives(object):
+    def save_links(self, url) :
+        '''this is save file'''
+        links = []
+        links_dict = {}
+        name = []
+        count = 0
+        try:
+            data = requests.get(url, timeout = 3)
+            content = data.text
+            link_pat = '"(ed2k://\|file\|[^"]+?\.(S\d+)(E\d+)[^"]+?1024X\d{3}[^"]+?)"'
+            name_pat = re.compile(r'<h2 class="entry_title">(.*?)</h2>', re.S)
+            links = set(re.findall(link_pat, content))
+            name = re.findall(name_pat, content)
+            count = len(links)
+        except Exception as ee:
+            print(ee)
+            pass
+        for i in links:
+            links_dict[int(i[1][1:3]) * 100 + int(i[2][1:3])] = i#把剧集按s和e提取编号
+            print(i)
+        try:
+            print (os.getcwd() + name[0].replace('/', ' '))
+            with open(file='D:\\CodeWorkspace\\videos\\' + name[0].replace('/', ' ') + '.txt', mode='a', encoding='utf-8') as f:
+            #with open('D:\\CodeWorkspace\\test.txt','a','utf8') as f:
+                for i in sorted(list(links_dict.keys())):#按季数+集数排序顺序写入
+                    f.write(links_dict[i][0] + '\n')
+            print("8 Get links ... ", name[0], count)
+        except Exception as ex:
+            print (ex)
+            pass
+
+    def get_urls(self):
+        try:
+            for i in range(2015, 25000):
+                base_url = 'http://cn163.net/archives/'
+                url = base_url + str(i) + '/'
+                if requests.get(url).status_code == 404:
+                    print ("error 404")
+                    continue
+                else:
+                    self.save_links(url)
+        except Exception as et:
+            print(et)
+            pass
+
+    def main(self):
+        thread1=threading.Thread(target=self.get_urls())
+        thread1.start()
+        thread1.join()
+
+if __name__ == '__main__':
+    startTime = time.time()
+    a = Archives()
+    a.main()
+    endTime = time.time()
+    print(endTime-startTime)
