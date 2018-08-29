e58f9f6655087d3b5fe1417cec2a44c74bb93051
lucas.wong <lucaswong@163.com>
2018-08-29 09:35:56 +0800

    new file

---
+++ CodeReview/download.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+"""
+-----------------------------------------------------------
+ Name：            ToolsProject/download
+ Purpose：         
+
+ Author：          lucas.wang
+
+ Created：         2018-08-03
+ Copyright：       (C) lucas.wang 2018
+ Licence:          MIT
+ ----------------------------------------------------------
+"""
+# ! /usr/bin/env python
+
+
+if __name__ == '__main__':
+    pass
---
+++ Crawler/download_novel.py
@@ -0,0 +1,18 @@
+# _*_ coding:utf-8 _*_
+"""
+-----------------------------------------------------------
+ Name：            ToolsProject/download_novel.py
+ Purpose：         
+
+ Author：          lucas.wang
+
+ Created：         2018-08-14
+ Copyright：       (C) lucas.wang 2018
+ Licence:          MIT
+ ----------------------------------------------------------
+"""
+# ! /usr/bin/env python
+
+
+if __name__ == '__main__':
+    pass
