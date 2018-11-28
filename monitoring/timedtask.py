5a15eb0c2d6f0e6fb297b4fc4176178e88b9d8b1
lucas.wong <lucaswong@163.com>
2018-11-28 15:43:16 +0800

    定时任务，以需要的时间间隔执行某个命令

--- monitoring/timedtask.py
+++ monitoring/timedtask.py
@@ -12,6 +12,24 @@
  ----------------------------------------------------------
 """
 # ! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/27 15:59
+# @Desc    : 定时任务，以需要的时间间隔执行某个命令
+# @File    : timedtask.py
+# @Software: PyCharm
+
+import time, os
+from monitorserver import alltask
+
+
+def roll_back(cmd, inc=60):
+    while True:
+        # 执行方法，函数
+        alltask()
+        time.sleep(inc)
+
+
+roll_back("echo %time%", 5)
 
 
 if __name__ == '__main__':
