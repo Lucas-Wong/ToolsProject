66666898536d02fe33a2bd10f4b6674c53561b45
lucas.wong <lucaswong@163.com>
2018-12-21 11:38:32 +0800

    Run AD

--- ExecRunAD/Run.py
+++ ExecRunAD/Run.py
@@ -13,6 +13,70 @@
 """
 # ! /usr/bin/env python
 
+import time
+import Run_AD
+import Monitor_AD
+import Global
+import logging
+import logging.config
+import os
+
+filepath = os.path.join(os.path.dirname(__file__), 'logging.conf')
+logging.config.fileConfig(filepath)
+logger_name = "AppName"
+logger = logging.getLogger(logger_name)
+
+class init_run(object):
+
+    def __init__(self):
+        pass
+
+    def run(self, is_all="other"):
+        run_ad = Run_AD.run_ad()
+        run_ad.main(is_all)
+
+        monitor_ad = Monitor_AD.monitor_ad()
+        monitor_ad.main()
+
+    def main(self):
+        t = time.localtime()
+        h = t.tm_hour
+        m = t.tm_min
+        s = t.tm_sec
+        w = time.strftime('%w', t)
+        logger.info(h, m, s, w)
+        time.sleep(0.3)
+        str_time = 12
+        str_time_2 = 14
+        is_friday = True
+        is_saturday = True
+        Global.set_is_run(False)
+
+        now = time.localtime()
+        while True:
+            if (now.tm_hour >= str_time and now.tm_hour < str_time_2) and is_friday:
+                logger.info("start run friday", str(now.tm_hour), ":", str(now.tm_min))
+                is_friday = False
+                self.run("all")
+                # return_value = self.friday()
+                # print("2************************")
+                # print(return_value)
+                # if return_value is None:
+                #     is_friday = True
+                #     time.sleep(2)
+
+            if now.tm_hour >= str_time_2 and is_saturday:
+                logger.info("start run saturday", str(now.tm_hour), ":", str(now.tm_min))
+                is_saturday = False
+                Global.set_comm_week(int(Global.get_comm_week()) + 1)
+                self.run()
+                # is_done = self.saturday()
+
+                # if is_done:
+                #     break
+
+            if Global.get_is_run():
+                break
 
 if __name__ == '__main__':
     pass
--- ExecRunAD/Run_AD.py
+++ ExecRunAD/Run_AD.py
@@ -13,6 +13,109 @@
 """
 # ! /usr/bin/env python
 
+import Global
+import json
+import time
+import Parameter
+import Execute
+import Global
+import logging
+import logging.config
+import os
+
+filepath = os.path.join(os.path.dirname(__file__), 'logging.conf')
+logging.config.fileConfig(filepath)
+logger_name = "AppName"
+logger = logging.getLogger(logger_name)
+
+class run_ad(object):
+
+    def __init__(self):
+        pass
+
+    def main(self, is_all):
+        """
+        Run AD main function
+        :param is_all:
+        :return:
+        """
+
+        Job_id_list = []
+        Job_done = 0
+        Job_total_count = {}
+
+        parameter_function = Parameter.parameter(Global.get_comm_week(), 0)
+
+        if str("all") == str(is_all):
+            run_country = parameter_function.AllCountry()
+            return_code = self.Exce(Global.get_master_1_url(), run_country)
+
+            run_ad_object = json.loads(return_code)
+
+            Job_id_list = [run_ad_object["data"]["jobExecutationId"]]
+            Job_done = 1
+            Job_total_count[run_ad_object["data"]["jobExecutationId"]] = run_ad_object["data"]["totalAdCount"]
+
+            logger.info(run_ad_object)
+        else:
+            run_country = parameter_function.JPN()
+            return_code = self.Exce(Global.get_master_1_url(), run_country)
+
+            run_ad_object = json.loads(return_code)
+
+            Job_id_list.append([run_ad_object["data"]["jobExecutationId"]])
+            Job_total_count[run_ad_object["data"]["jobExecutationId"]] = run_ad_object["data"]["totalAdCount"]
+
+            logger.info(run_ad_object)
+
+            run_country = parameter_function.CHN()
+            return_code = self.Exce(Global.get_master_2_url(), run_country)
+
+            run_ad_object = json.loads(return_code)
+
+            Job_id_list.append([run_ad_object["data"]["jobExecutationId"]])
+            Job_total_count[run_ad_object["data"]["jobExecutationId"]] = run_ad_object["data"]["totalAdCount"]
+
+            logger.info(run_ad_object)
+
+            run_country = parameter_function.USA()
+            return_code = self.Exce(Global.get_master_2_url(), run_country)
+
+            run_ad_object = json.loads(return_code)
+
+            Job_id_list.append([run_ad_object["data"]["jobExecutationId"]])
+            Job_total_count[run_ad_object["data"]["jobExecutationId"]] = run_ad_object["data"]["totalAdCount"]
+
+            logger.info(run_ad_object)
+
+            run_country = parameter_function.Other()
+            return_code = self.Exce(Global.get_master_2_url(), run_country)
+
+            run_ad_object = json.loads(return_code)
+
+            Job_id_list.append([run_ad_object["data"]["jobExecutationId"]])
+            Job_total_count[run_ad_object["data"]["jobExecutationId"]] = run_ad_object["data"]["totalAdCount"]
+
+            logger.info(run_ad_object)
+
+            Job_done = 4
+
+        Global.set_job_id_list(Job_id_list)
+        Global.set_job_done(Job_done)
+        Global.set_job_total_count(Job_total_count)
+
+
+    def Exce(self, url, run_country):
+        run_job = Execute.execute(url)
+        response = run_job.post(run_country)
+        logger.info("1************************")
+        logger.info(response.status_code)
+
+        if response.status_code is not 200:
+            time.sleep(30)
+            self.Exce(url, run_country)
+        else:
+            return json.loads(response.text)["string"]
 
 if __name__ == '__main__':
     pass
